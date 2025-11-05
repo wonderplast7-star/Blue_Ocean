501dceddbaecaf7b78a50666a99909493550e2e5
wonderplast7-star <wonderplast7@gmail.com>
2025-11-05 17:59:45 +0530

    fixing error

--- src/component/Brand/Brand2.jsx
+++ src/component/Brand/Brand2.jsx
@@ -3,14 +3,14 @@ import Hero_brand_2 from "../Brands/Hero_brand_2";
 import Story_brand_2 from "../Brands/Story_brand_2";
 import WebsiteBrand3 from "../Brands/website_brand_3.jsx";
 import { useTheme } from '../../contexts/ThemeContext';
-import ScrollToTop from "../Scrolltotop.jsx";
+import Scrolltotop from "../Scrolltotop.jsx";
 
 export default function Brand2() {
   const { isDarkMode } = useTheme();
   
   return (
     <div className={isDarkMode ? 'dark bg-gray-900 text-white' : 'light bg-gray-50 text-gray-900'}>
-        <ScrollToTop/>
+        <Scrolltotop/>
      <Hero_brand_2 />
      <Story_brand_2 />
      <WebsiteBrand3 />
